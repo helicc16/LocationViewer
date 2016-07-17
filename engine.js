075d2bf6daf20f4efe0a41edbff87dac4c974fd4
helicc16 <kiennd53@gmail.com>
2016-07-17 10:42:29 -0400

    makes changes to port and ip address for publishing on redhat openshift

--- engine.js
+++ engine.js
@@ -1,7 +1,8 @@
 //Declaring top-level variables:
 var getUpdatesBtn_clicked = false;
-
-var socket = io.connect(':');
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var socket = io.connect(server_ip_address + ':' + server_port);
 
 $(document).ready(function () {
 
